86fd0eb890758a3ffb19b53068dd3be95a569d97
Thibault <Thibault.ahn@gmail.com>
2025-06-10 09:50:31 +0200

    Update disk usage script to use Get-CimInstance

--- scripts/DiskUsageReport.ps1
+++ scripts/DiskUsageReport.ps1
@@ -3,7 +3,7 @@
     Generates a disk usage report for local drives.
 #>
 
-Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3" |
+Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DriveType=3" |
     Select-Object DeviceID,
                   @{Name="SizeGB";Expression={[math]::round($_.Size/1GB,2)}},
                   @{Name="FreeGB";Expression={[math]::round($_.FreeSpace/1GB,2)}},
