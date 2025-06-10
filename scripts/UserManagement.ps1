00c3fbe019309d2b6ad9fc9f551952d8b1a8c03d
Thibault <Thibault.ahn@gmail.com>
2025-06-10 09:48:09 +0200

    Check user existence before creation or deletion

--- scripts/UserManagement.ps1
+++ scripts/UserManagement.ps1
@@ -16,12 +16,20 @@ switch ($Action.ToLower()) {
             Write-Error 'UserName and Password are required to create a user.'
             break
         }
+        if (Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue) {
+            Write-Error "User '$UserName' already exists."
+            break
+        }
         $secure = ConvertTo-SecureString $Password -AsPlainText -Force
         New-LocalUser -Name $UserName -Password $secure
     }
     'delete' {
         if (-not $UserName) {
             Write-Error 'UserName is required to delete a user.'
+            break
+        }
+        if (-not (Get-LocalUser -Name $UserName -ErrorAction SilentlyContinue)) {
+            Write-Error "User '$UserName' does not exist."
             break
         }
         Remove-LocalUser -Name $UserName
